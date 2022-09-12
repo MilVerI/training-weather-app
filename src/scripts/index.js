4acdee4f0866ee94220d7469335485e4ded74ee9
VeroNika <vero.milevska@gmail.com>
2022-09-12 22:11:57 +0300

    Work process

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,9 +14,11 @@ function infoOnCurrentLocation() {
 function handlePosition(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
-  let apiByGeo = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 
-  axios.get(apiByGeo).then(showCurrentGeoData);
+  window.location.href = `/result?lat=${lat}&lon=${lon}`;
+  //   let apiByGeo = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+
+  //   axios.get(apiByGeo).then(showCurrentGeoData);
 }
 
 //показ прогнозу на поточний день для поточної локації користувача
@@ -56,27 +58,6 @@ function formatDay1(date) {
   }
 }
 
-//функція конвертації поточного часу після відкриття сторінки юзером
-function formatTime(date) {
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-
-  if (minutes < 10) {
-    formattedTime = `${hours}:0${minutes}`;
-    return formattedTime;
-  } else {
-    formattedTime = `${hours}:${minutes}`;
-    return formattedTime;
-  }
-}
-
-function setDateTime(day, time) {
-  let thisDay = document.querySelector("#firstDayName");
-  thisDay.innerHTML = day;
-  let thisTime = document.querySelector("#time");
-  thisTime.innerHTML = time;
-}
-
 //фукнція отримання назви міста з пошукового рядка
 function letCity(event) {
   event.preventDefault();
@@ -89,83 +70,6 @@ function letCity(event) {
   }
 }
 
-//функція зміни шкали для температури з Цельсія на Фаренгейти і навпаким; потребує покращення
-function changeTempMode() {
-  if (setMode.getAttribute("class") === "celsius") {
-    let temp1 = document.querySelector("#firstDayTemp");
-    let temp2 = document.querySelector("#secondDayTemp");
-    let temp3 = document.querySelector("#thirdDayTemp");
-    let temp4 = document.querySelector("#fourthDayTemp");
-    let temp5 = document.querySelector("#fifthDayTemp");
-    let temp6 = document.querySelector("#sixthDayTemp");
-
-    let temp1Value = temp1.innerHTML;
-    let farTemp1 = Math.round(temp1Value * 1.8 + 32);
-    temp1.innerHTML = `${farTemp1}`;
-
-    let temp2Value = temp2.innerHTML;
-    let farTemp2 = Math.round(temp2Value * 1.8 + 32);
-    temp2.innerHTML = `${farTemp2}`;
-
-    let temp3Value = temp3.innerHTML;
-    let farTemp3 = Math.round(temp3Value * 1.8 + 32);
-    temp3.innerHTML = `${farTemp3}`;
-
-    let temp4Value = temp4.innerHTML;
-    let farTemp4 = Math.round(temp4Value * 1.8 + 32);
-    temp4.innerHTML = `${farTemp4}`;
-
-    let temp5Value = temp5.innerHTML;
-    let farTemp5 = Math.round(temp5Value * 1.8 + 32);
-    temp5.innerHTML = `${farTemp5}`;
-
-    let temp6Value = temp6.innerHTML;
-    let farTemp6 = Math.round(temp6Value * 1.8 + 32);
-    temp6.innerHTML = `${farTemp6}`;
-
-    setMode.innerHTML = "°F";
-    setMode.setAttribute("class", "fahrenheit");
-  } else {
-    if (setMode.getAttribute("class") === "fahrenheit") {
-      let temp1 = document.querySelector("#firstDayTemp");
-      let temp2 = document.querySelector("#secondDayTemp");
-      let temp3 = document.querySelector("#thirdDayTemp");
-      let temp4 = document.querySelector("#fourthDayTemp");
-      let temp5 = document.querySelector("#fifthDayTemp");
-      let temp6 = document.querySelector("#sixthDayTemp");
-
-      let temp1Value = temp1.innerHTML;
-      let celsTemp1 = Math.round((temp1Value - 32) / 1.8);
-      temp1.innerHTML = `${celsTemp1}`;
-
-      let temp2Value = temp2.innerHTML;
-      let celsTemp2 = Math.round((temp2Value - 32) / 1.8);
-      temp2.innerHTML = `${celsTemp2}`;
-
-      let temp3Value = temp3.innerHTML;
-      let celsTemp3 = Math.round((temp3Value - 32) / 1.8);
-      temp3.innerHTML = `${celsTemp3}`;
-
-      let temp4Value = temp4.innerHTML;
-      let celsTemp4 = Math.round((temp4Value - 32) / 1.8);
-      temp4.innerHTML = `${celsTemp4}`;
-
-      let temp5Value = temp5.innerHTML;
-      let celsTemp5 = Math.round((temp5Value - 32) / 1.8);
-      temp5.innerHTML = `${celsTemp5}`;
-
-      let temp6Value = temp6.innerHTML;
-      let celsTemp6 = Math.round((temp6Value - 32) / 1.8);
-      temp6.innerHTML = `${celsTemp6}`;
-
-      setMode.innerHTML = "°C";
-      setMode.setAttribute("class", "celsius");
-    } else {
-      alert("Something went wrong. Reload the page");
-    }
-  }
-}
-
 //зміна шкали температур на стандартну (Цельсії), отримання назви міста з пошукового рядка
 // і перехід до виконання showTodayTemp
 function getCityData(cityName) {
@@ -174,49 +78,52 @@ function getCityData(cityName) {
   if (setMode.getAttribute("class") === "fahrenheit") {
     changeTempMode();
   }
-debugger;
 
-axios
-  .get(apiCity)
-  .then((response) => {
-    localStorage.setItem("apiData", JSON.stringify(response.data));
-  })
-  .catch((error) => {
-    alert(`Sorry, "${cityName}" cannot be found. Check the city and retry`);
-  });
+  axios
+    .get(apiCity)
+    .then((response) => {
+      localStorage.setItem("apiData", JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      alert(`Sorry, "${cityName}" cannot be found. Check the city and retry`);
+    }); //потрібно більш чітко відловлювати помилки
 
-let data = JSON.parse(localStorage.getItem("apiData"));
-//console.log(data);
+  let todayResponsData = JSON.parse(localStorage.getItem("apiData"));
 
-showTodayTemp(data);
-datetimeInCity(data);
+  showTodayTemp(todayResponsData);
+  //datetimeInCity(todayResponsData);
+}
 
- // axios
-   // .get(apiCity)
-   // .then(showTodayTemp)
-    // .catch((error) => {
-    //   alert(`Sorry, "${cityName}" cannot be found. Check the city and retry`);
-    // })
+//Получение параметров
+function getCityNameBySearchParams() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const city = urlParams.get("city");
+  if (!city) return false;
+  return city;
+}
+
+function getCoorsBySearchParams() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const lat = urlParams.get("lat");
+  const lng = urlParams.get("lng");
+  if (!lat || !lng) return false;
+  return { lng, lat };
+}
+
+//Проверяем параметри строки
+function startWithParams() {
+  const city = getCityNameBySearchParams();
+  const coords = getCoorsBySearchParams();
+  if (city) {
+    //Тут запускаем функцию по городу
+  } else if (coords) {
+    //Тут делаем запрос по координатам
+  } else {
+    //редиректим на титуьлную
+    window.history.replace("/");
   }
-
-  function datetimeInCity(data) {
-    console.log(data.dt * 1000);
-  };
-  
-//зміна назви міста та температури на сторінці для поточного дня
-function showTodayTemp(data) {
-  //formatDay1(response.data.dt);
-  let tempData = Math.round(data.main.temp);
-  let temp = document.querySelector("#firstDayTemp");
-  let city = document.querySelector("#current-city");
-  temp.innerHTML = `${tempData}`;
-  city.innerHTML = inputCityName;
 }
 
 infoOnCurrentLocation();
-formatDay1(dateTime);
-formatTime(dateTime);
-setDateTime(formattedDay1, formattedTime);
 let searchForm = document.querySelector("#searching");
 searchForm.addEventListener("submit", letCity);
-setMode.addEventListener("click", changeTempMode);
